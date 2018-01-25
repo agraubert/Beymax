631c96d607c02ea006a8cfb42dc3a76c625a252e
Aaron Graubert <captianjroot@live.com>
2018-01-25 14:31:13 -0500

    Update_overwatch is dispatched via events, not direct call

--- bots/ow.py
+++ bots/ow.py
@@ -115,7 +115,7 @@ def EnableOverwatch(bot):
 
     @bot.add_command('!owupdate')
     async def cmd_update(self, message, content):
-        await update_overwatch(self)
+        self.dispatch('task:update_overwatch')
 
     @bot.add_command('!ow')
     async def cmd_ow(self, message, content):
@@ -147,7 +147,7 @@ def EnableOverwatch(bot):
                     "Alright! I'll keep track of your stats"
                 )
                 await asyncio.sleep(15)
-                await update_overwatch(self)
+                self.dispatch('task:update_overwatch')
             except:
                 await self.send_message(
                     message.channel,
